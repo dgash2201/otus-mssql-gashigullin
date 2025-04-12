8e2e1a497c62af2f5c154d622c31ae221fdf84df
Danil <dgash2201@yandex.ru>
2025-04-12 11:11:55 +0300

    HW19 сделать последнюю партицию пустой

--- HW19-partition/hw_partitioning.sql
+++ HW19-partition/hw_partitioning.sql
@@ -10,17 +10,18 @@ alter database WideWorldImporters add file (name = 'ct2013', filename = 'C:\Test
 alter database WideWorldImporters add file (name = 'ct2014', filename = 'C:\Test\ct2014.ndf') to filegroup CT_2014
 alter database WideWorldImporters add file (name = 'ct2015', filename = 'C:\Test\ct2015.ndf') to filegroup CT_2015
 alter database WideWorldImporters add file (name = 'ct2016', filename = 'C:\Test\ct2016.ndf') to filegroup CT_2016
+alter database WideWorldImporters add file (name = 'ct2017', filename = 'C:\Test\ct2017.ndf') to filegroup CT_2017
 go
 
 create partition function pf_dt(date)
 as
-	range right for values ('2014-01-01','2015-01-01','2016-01-01')
+	range right for values ('2014-01-01','2015-01-01','2016-01-01', '2017-01-01')
 go
 
 
 create partition scheme ps_dt
 as
-	partition pf_dt to (CT_2013,CT_2014,CT_2015,CT_2016)
+	partition pf_dt to (CT_2013,CT_2014,CT_2015,CT_2016, CT_2017)
 go
 
 create clustered index CX_Sales_CustomerTransactions
